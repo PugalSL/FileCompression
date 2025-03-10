ae998ffa302ff1468f0f8f1b0e1189467edf0ede
PugalSL <spugalsurendar001@gmail.com>
2025-03-11 00:52:55 +0530

    jar file created

--- src/CompressorUI.java
+++ src/CompressorUI.java
@@ -3,47 +3,43 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CompressorUI extends JFrame
-{
+public class CompressorUI extends JFrame {
     private CardLayout cardLayout;
     private JPanel mainPanel;
     private JPanel homePanel, compressPanel, decompressPanel;
 
     private JTextField sourceFileField = new JTextField(35);
     private JTextField destinationFileField = new JTextField(45);
-
     private JTextField sourceFileField1 = new JTextField(45);
     private JTextField destinationFileField1 = new JTextField(45);
 
-    public CompressorUI() throws HeadlessException
-    {
+    public CompressorUI() {
         super("File Compressor");
-        setBounds(100, 100, 750, 150);
+        setBounds(100, 100, 750, 200);
 
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
 
-        homePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 50)); // FlowLayout with spacing
-        JButton compressButton = new JButton("Compress a File");
+        // Home Panel
+        homePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 50));
+        JButton compressButton = new JButton("Compress a File      ");
         JButton decompressButton = new JButton("Decompress a File");
         homePanel.add(compressButton);
         homePanel.add(decompressButton);
 
+        // Compress Panel
         compressPanel = new JPanel();
         compressPanel.setLayout(new FlowLayout());
-
-        decompressPanel = new JPanel();
-        decompressPanel.setLayout(new FlowLayout());
-
         JButton sourceFileChooser = new JButton("Browse");
         JButton destinationFileChooser = new JButton("Browse");
         JLabel sourceFileLabel = new JLabel("Source File Name: ");
         JLabel destinationFileLabel = new JLabel("Destination Folder Name: ");
         JButton compress = new JButton("Compress");
+        JButton backButton1 = new JButton("Go Back");
         JLabel message = new JLabel("");
         message.setForeground(new Color(18, 181, 34));
         compress.setBackground(new Color(69, 64, 248));
-        sourceFileField.setFont(new Font("Verdana", Font.ITALIC, 16));
+
         compressPanel.add(sourceFileLabel);
         compressPanel.add(sourceFileField);
         compressPanel.add(sourceFileChooser);
@@ -52,16 +48,21 @@ public class CompressorUI extends JFrame
         compressPanel.add(destinationFileChooser);
         compressPanel.add(compress);
         compressPanel.add(message);
+        compressPanel.add(backButton1);
 
+        // Decompress Panel
+        decompressPanel = new JPanel();
+        decompressPanel.setLayout(new FlowLayout());
         JButton sourceFileChooser1 = new JButton("Browse");
         JButton destinationFileChooser1 = new JButton("Browse");
         JLabel sourceFileLabel1 = new JLabel("Source File Name: ");
         JLabel destinationFileLabel1 = new JLabel("Destination Folder Name: ");
         JButton decompress = new JButton("Decompress");
+        JButton backButton2 = new JButton("Go Back");
         JLabel message1 = new JLabel("");
         message1.setForeground(new Color(21, 67, 129));
         decompress.setBackground(new Color(69, 64, 248));
-        sourceFileField.setFont(new Font("Verdana", Font.ITALIC, 16));
+
         decompressPanel.add(sourceFileLabel1);
         decompressPanel.add(sourceFileField1);
         decompressPanel.add(sourceFileChooser1);
@@ -70,55 +71,18 @@ public class CompressorUI extends JFrame
         decompressPanel.add(destinationFileChooser1);
         decompressPanel.add(decompress);
         decompressPanel.add(message1);
+        decompressPanel.add(backButton2);
 
         // Compress action listener
         compress.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String sourcePath = sourceFileField.getText();
-                String base  =  sourcePath.substring(0,sourcePath.lastIndexOf('.'));
+                String base = sourcePath.substring(0, sourcePath.lastIndexOf('.'));
                 base = base.substring(base.lastIndexOf('\\') + 1);
-                String destPath = destinationFileField.getText() + "\\" + base+ "-comp.txt";
+                String destPath = destinationFileField.getText() + "\\" + base + "-comp.txt";
                 HuffCompression.compress(sourcePath, destPath);
                 message.setText("Compressed successfully");
-            }
-        });
-
-        // Browse for source file for compression
-        sourceFileChooser.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                JFileChooser jfc = new JFileChooser("D:\\Java");
-                int userChoice = jfc.showOpenDialog(CompressorUI.this);
-                if (userChoice == JFileChooser.APPROVE_OPTION)
-                {
-                    String selectedFilePath = jfc.getSelectedFile().getPath();
-                    sourceFileField.setText(selectedFilePath);
-                }
-                if (userChoice == JFileChooser.CANCEL_OPTION)
-                    sourceFileField.setText("No File Selected");
-            }
-        });
-
-        // Browse for destination folder for compression
-        destinationFileChooser.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                JFileChooser jfc = new JFileChooser("D:\\Java");
-                jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int userChoice = jfc.showOpenDialog(CompressorUI.this);
-
-                if (userChoice == JFileChooser.APPROVE_OPTION)
-                {
-                    String selectedFolderPath = jfc.getSelectedFile().getPath();
-                    destinationFileField.setText(selectedFolderPath);
-                }
-                else if (userChoice == JFileChooser.CANCEL_OPTION)
-                {
-                    destinationFileField.setText("No Folder Selected");
-                }
             }
         });
 
@@ -127,49 +91,61 @@ public class CompressorUI extends JFrame
             @Override
             public void actionPerformed(ActionEvent e) {
                 String sourcePath = sourceFileField1.getText();
-                String base = sourcePath.substring(0,sourcePath.lastIndexOf('-'));
-                String base1 = base.substring(base.lastIndexOf('\\')+1);
-                String destPath = destinationFileField1.getText() + "\\" + base1+ "-decomp.txt";
+                String base = sourcePath.substring(0, sourcePath.lastIndexOf('-'));
+                String base1 = base.substring(base.lastIndexOf('\\') + 1);
+                String destPath = destinationFileField1.getText() + "\\" + base1 + "-decomp.txt";
                 HuffCompression.decompress(sourcePath, destPath);
                 message1.setText("Decompressed successfully");
             }
         });
 
+        // "Go Back" Button Listeners
+        backButton1.addActionListener(e -> cardLayout.show(mainPanel, "Home"));
+        backButton2.addActionListener(e -> cardLayout.show(mainPanel, "Home"));
+
+        // Browse for source file for compression
+        sourceFileChooser.addActionListener(e -> {
+            JFileChooser jfc = new JFileChooser("D:\\Java");
+            int userChoice = jfc.showOpenDialog(CompressorUI.this);
+            if (userChoice == JFileChooser.APPROVE_OPTION) {
+                sourceFileField.setText(jfc.getSelectedFile().getPath());
+            } else {
+                sourceFileField.setText("No File Selected");
+            }
+        });
+
+        // Browse for destination folder for compression
+        destinationFileChooser.addActionListener(e -> {
+            JFileChooser jfc = new JFileChooser("D:\\Java");
+            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int userChoice = jfc.showOpenDialog(CompressorUI.this);
+            if (userChoice == JFileChooser.APPROVE_OPTION) {
+                destinationFileField.setText(jfc.getSelectedFile().getPath());
+            } else {
+                destinationFileField.setText("No Folder Selected");
+            }
+        });
+
         // Browse for source file for decompression
-        sourceFileChooser1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                JFileChooser jfc = new JFileChooser("D:\\Java");
-                int userChoice = jfc.showOpenDialog(CompressorUI.this);
-                if (userChoice == JFileChooser.APPROVE_OPTION)
-                {
-                    String selectedFilePath = jfc.getSelectedFile().getPath();
-                    sourceFileField1.setText(selectedFilePath);
-                }
-                if (userChoice == JFileChooser.CANCEL_OPTION)
-                    sourceFileField1.setText("No File Selected");
+        sourceFileChooser1.addActionListener(e -> {
+            JFileChooser jfc = new JFileChooser("D:\\Java");
+            int userChoice = jfc.showOpenDialog(CompressorUI.this);
+            if (userChoice == JFileChooser.APPROVE_OPTION) {
+                sourceFileField1.setText(jfc.getSelectedFile().getPath());
+            } else {
+                sourceFileField1.setText("No File Selected");
             }
         });
 
         // Browse for destination folder for decompression
-        destinationFileChooser1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                JFileChooser jfc = new JFileChooser("D:\\Java");
-                jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int userChoice = jfc.showOpenDialog(CompressorUI.this);
-
-                if (userChoice == JFileChooser.APPROVE_OPTION)
-                {
-                    String selectedFolderPath = jfc.getSelectedFile().getPath();
-                    destinationFileField1.setText(selectedFolderPath);
-                }
-                else if (userChoice == JFileChooser.CANCEL_OPTION)
-                {
-                    destinationFileField1.setText("No Folder Selected");
-                }
+        destinationFileChooser1.addActionListener(e -> {
+            JFileChooser jfc = new JFileChooser("D:\\Java");
+            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int userChoice = jfc.showOpenDialog(CompressorUI.this);
+            if (userChoice == JFileChooser.APPROVE_OPTION) {
+                destinationFileField1.setText(jfc.getSelectedFile().getPath());
+            } else {
+                destinationFileField1.setText("No Folder Selected");
             }
         });
 
@@ -179,21 +155,14 @@ public class CompressorUI extends JFrame
 
         cardLayout.show(mainPanel, "Home");
 
-        compressButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cardLayout.show(mainPanel, "Compress");
-            }
-        });
-
-        decompressButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cardLayout.show(mainPanel, "Decompress");
-            }
-        });
+        compressButton.addActionListener(e -> cardLayout.show(mainPanel, "Compress"));
+        decompressButton.addActionListener(e -> cardLayout.show(mainPanel, "Decompress"));
 
         add(mainPanel);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(CompressorUI::new);
     }
 }
